bcdc6218a82f61de436db45a50acf980f0cf381e
MultiCampus1102 <dlrlftjs12@gmail.com>
2017-03-17 17:18:29 +0900

    어헣헣

--- GitTest/src/HelloWorld.java
+++ GitTest/src/HelloWorld.java
@@ -1,9 +1,6 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("안녕세계");
-		System.out.println("안녕세계");
-		System.out.println("안녕시발");
-		System.out.println("안녕네계");
+		System.out.println("어헣헣");
 	}
 }
